e13c13420871e2c269cf7b2c3d16582d8a042203
ThiyakuAbi20 <abiramythiyakesan280@gmail.com>
2025-04-26 09:31:52 +0530

    commentpage_backend

--- eduposts/src/main/java/com/example/eduposts/repository/CommentRepository.java
+++ eduposts/src/main/java/com/example/eduposts/repository/CommentRepository.java
@@ -1,15 +1,5 @@
-//package com.example.eduposts.repository;
-//
-//import com.example.eduposts.model.Comment;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//public interface CommentRepository extends JpaRepository<Comment, Long> {
-//    List<Comment> findByPostId(Long postId);
-//}
+
+
 package com.example.eduposts.repository;
 
 import com.example.eduposts.model.Comment;
